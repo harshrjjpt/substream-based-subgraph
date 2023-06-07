1aa121636c49c561a67eaf5b6ba0e920787377ed
harshrjjpt <harshrjjpt@gmail.com>
2023-06-07 16:08:14 +0530

    deployment

--- target/wasm32-unknown-unknown/release/deps/substreams.d
+++ target/wasm32-unknown-unknown/release/deps/substreams.d
@@ -1,6 +1,6 @@
-/home/harshrjjpt/Desktop/DappLooker/dapplooker-substreams/substreams/network_substream/target/wasm32-unknown-unknown/release/deps/substreams.wasm: src/lib.rs src/key.rs src/pb/mod.rs src/db.rs src/tables.rs src/pb/acme.rs
+/home/harshrjjpt/Desktop/learn_blockchainn/substream-based-subgraph/target/wasm32-unknown-unknown/release/deps/substreams.wasm: src/lib.rs src/key.rs src/pb/mod.rs src/db.rs src/tables.rs src/pb/acme.rs
 
-/home/harshrjjpt/Desktop/DappLooker/dapplooker-substreams/substreams/network_substream/target/wasm32-unknown-unknown/release/deps/substreams.d: src/lib.rs src/key.rs src/pb/mod.rs src/db.rs src/tables.rs src/pb/acme.rs
+/home/harshrjjpt/Desktop/learn_blockchainn/substream-based-subgraph/target/wasm32-unknown-unknown/release/deps/substreams.d: src/lib.rs src/key.rs src/pb/mod.rs src/db.rs src/tables.rs src/pb/acme.rs
 
 src/lib.rs:
 src/key.rs:
